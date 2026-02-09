aa1d93d4fb1bb0bb5284ffb247e91e9282b17e76
krishnaprasad c s <krishnaprasadcsofficial@gmail.com>
2026-02-09 10:46:03 +0530

    commit

--- Back-end/jest.config.js
+++ Back-end/jest.config.js
@@ -3,5 +3,16 @@ export default {
   transform: {},
   setupFiles: ['<rootDir>/server/tests/setupEnv.js'],
   testTimeout: 300000,
-  verbose: true
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'server/**/*.js',
+    '!server/tests/**',
+    '!server/node_modules/**',
+    '!server/config/**',
+    '!server/coverage/**',
+    '!**/node_modules/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov']
 };
